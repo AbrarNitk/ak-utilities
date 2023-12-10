28d90b244d1e11a48b8854055cc6f3a1aadd58e9
AbrarNitk <abrar.nitk@gmail.com>
2023-12-10 18:11:07 +0530

    random word generation

--- src/rand/string.rs
+++ src/rand/string.rs
@@ -1,33 +1,31 @@
 // const LETTERS: &[char] = letters();
 
-use rand::Rng;
-
-fn letters() {
-    let chars = ('a'..='z').chain('A'..='B').chain('0'..='9');
+fn letters() -> Vec<char> {
+    ('a'..='z').chain('A'..='B').chain('0'..='9').collect()
 }
 
 pub struct CharIter {
     rng: rand::rngs::ThreadRng,
+    chars: Vec<char>,
 }
 
 impl CharIter {
     fn new() -> Self {
         Self {
-            rng: rand::thread_rng()
+            rng: rand::thread_rng(),
+            chars: letters(),
         }
-    }
-
-    fn iter(self) -> Self {
-        CharIter::new()
     }
 }
 
 impl Iterator for CharIter {
-    type Item = u8;
+    type Item = char;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let t = Some(self.rng.gen());
-        t
+        use rand::Rng;
+        let idx: usize = self.rng.gen_range(0..self.chars.len());
+        let c = self.chars[idx];
+        Some(c)
     }
 }
 
@@ -45,6 +43,11 @@ pub struct Word<H> {
     len: H,
 }
 
+// pub struct WordIter<'a, H> {
+//     w: &'a Word<H>,
+//     char_iter: CharIter,
+// }
+
 impl<H> Word<H> {
     fn new(r: H) -> Self {
         Word {
@@ -53,9 +56,21 @@ impl<H> Word<H> {
     }
 }
 
+// impl<'a, H> Iterator for WordIter<'a, H> where H: NumericBound + Copy {
+//     type Item = String;
+//
+//     fn next(&mut self) -> Option<Self::Item> {
+//         let word_len = self.w.rand_len();
+//         let char_vector: Vec<char> = self.char_iter.take(word_len).collect();
+//         //let w: String = char_vector.iter().collect();
+//         //println!("word {} {} {}", w, char_vector.len(), word_len);
+//         None
+//     }
+// }
+
 impl<Idx> WordLen for Word<std::ops::Range<Idx>>
     where
-        Idx: NumericBound
+        Idx: NumericBound + Copy
         + PartialOrd
         + rand::distributions::uniform::SampleUniform,
 {
@@ -64,7 +79,7 @@ impl<Idx> WordLen for Word<std::ops::Range<Idx>>
         use rand::Rng;
         let mut rng = rand::thread_rng();
         let random_number: Self::Item =
-            rng.gen_range(Self::Item::min_value()..=Self::Item::max_value());
+            rng.gen_range(self.len.start..=self.len.end);
         random_number
     }
 }
@@ -76,16 +91,21 @@ impl<Idx> WordLen for Word<Idx> where Idx: NumericBound + Copy {
     }
 }
 
+
 pub fn word() {
-    let word = Word::new(1u8..9);
+    let word = Word::new(5u8..15);
     let word_len = word.rand_len();
     let char_iter = CharIter::new();
-    let char_vector: Vec<u8> = char_iter.iter().take(word_len as usize).collect();
-    let w: String = unsafe { String::from_utf8_unchecked(char_vector) };
-    println!("word {}", w);
+    let char_vector: Vec<char> = char_iter.take(word_len as usize).collect();
+    let w: String = char_vector.iter().collect();
+    println!("word {} {} {}", w, char_vector.len(), word_len);
 
-    let word = Word::new(1u8);
-    let l = word.rand_len();
+    let word = Word::new(6u8);
+    let word_len = word.rand_len();
+    let char_iter = CharIter::new();
+    let char_vector: Vec<char> = char_iter.take(word_len as usize).collect();
+    let w: String = char_vector.iter().collect();
+    println!("word {} {} {}", w, char_vector.len(), word_len);
 }
 
 impl NumericBound for u8 {
